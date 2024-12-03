f43d7d95752337851a3597ba5a741ddb8bf014cd
auroraphtgrp01 <sliverknight4869@gmail.com>
2024-12-03 12:42:10 +0700

    chore: increase buffer size dump

--- lcdp-app/scripts/dump.mjs
+++ lcdp-app/scripts/dump.mjs
@@ -1,21 +1,39 @@
 import 'dotenv/config';
-import { exec } from 'child_process';
-import { mkdir } from 'fs/promises';
+import {
+  exec
+} from 'child_process';
+import {
+  mkdir
+} from 'fs/promises';
 import fs from 'fs';
 import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import {
+  fileURLToPath
+} from 'url';
+import {
+  dirname
+} from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(
+  import.meta.url);
 const __dirname = dirname(__filename);
 const dataDir = path.join(__dirname, '../data');
 const outputFile = path.join(dataDir, 'dump-schema.sql');
 
 async function dumpDatabase() {
   try {
-    await mkdir(dataDir, { recursive: true });
+    await mkdir(dataDir, {
+      recursive: true
+    });
+
     const command = `docker exec -e PGPASSWORD=${process.env.DB_PASSWORD} postgres-5439 pg_dump -v -c --if-exists -F p -U ${process.env.DB_USER} -d ${process.env.DB_DATABASE}`;
-    exec(command, { shell: true }, (error, stdout, stderr) => {
+
+    const outputStream = fs.createWriteStream(outputFile);
+
+    exec(command, {
+      shell: true,
+      maxBuffer: 1024 * 1024 * 10
+    }, (error, stdout, stderr) => {
       if (error) {
         console.error(`Error executing command: ${error.message}`);
         return;
@@ -24,7 +42,15 @@ async function dumpDatabase() {
         console.error(`Error in stderr: ${stderr}`);
         return;
       }
-    }).stdout.pipe(fs.createWriteStream(outputFile));
+    }).stdout.pipe(outputStream);
+
+    outputStream.on('finish', () => {
+      console.log('Database dump completed successfully.');
+    });
+
+    outputStream.on('error', (err) => {
+      console.error('Error writing to file:', err);
+    });
   } catch (error) {
     console.error(`Failed to dump database: ${error.message}`);
     process.exit(1);
