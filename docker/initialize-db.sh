bd6551a119a7b1302cbe06b267caea20a551056a
auroraphtgrp01 <sliverknight4869@gmail.com>
2024-12-04 18:47:07 +0700

    fix: permission sh

--- docker/initialize-db.sh
+++ docker/initialize-db.sh
@@ -2,16 +2,16 @@
 chmod +x initialize-db.sh
 set -e
 
-export PGPASSWORD="$POSTGRESQL_PASSWORD"
-
-until pg_isready -h localhost -p 5432 -U "$POSTGRESQL_USERNAME"; do
-  echo "Waiting for PostgreSQL to be ready..."
-  sleep 2
+# Đợi PostgreSQL khởi động
+until pg_isready -h localhost -p 5432 -U postgres; do
+    echo "Waiting for PostgreSQL to be ready..."
+    sleep 2
 done
 
-psql -h localhost \
-     -U "$POSTGRESQL_USERNAME" \
-     -d "$POSTGRESQL_DATABASE" \
-     -f /docker-entrypoint-initdb.d/initialize-db.sql
+# Thêm một khoảng thời gian chờ ngắn
+sleep 5
+
+echo "Initializing database..."
+PGPASSWORD=postgres psql -h localhost -U postgres -d nocobase -f /docker-entrypoint-initdb.d/initialize-db.sql
 
 echo "Database initialized successfully"
