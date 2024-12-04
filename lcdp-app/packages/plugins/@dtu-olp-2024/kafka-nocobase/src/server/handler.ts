2cd6472f0a456ea981d44bde428585cae6ef3541
auroraphtgrp01 <sliverknight4869@gmail.com>
2024-12-04 21:15:12 +0700

    feat: handle event from kafka

--- lcdp-app/packages/plugins/@dtu-olp-2024/kafka-nocobase/src/server/handler.ts
+++ lcdp-app/packages/plugins/@dtu-olp-2024/kafka-nocobase/src/server/handler.ts
@@ -6,25 +6,77 @@ export class MessageHandlers {
     appInstance: Application<DefaultState, DefaultContext>
   ) {
     const data = message?.data;
+    const transferTransaction = data?.transferTransaction ?? [];
+    const receiverTransaction = data?.receiverTransaction ?? [];
     const transactionRepository =
       await appInstance.db.getRepository('transactions');
-    const transaction = await Promise.all(
-      data.map(async (transactionData: any) => {
+
+    const existingTransferTransactions = await transactionRepository.find({
+      where: {
+        transaction_code: transferTransaction.map(
+          (item: any) => item.transaction_id
+        ),
+      },
+    });
+
+    const existingReceiverTransactions = await transactionRepository.find({
+      where: {
+        transaction_code: receiverTransaction.map(
+          (item: any) => item.transaction_id
+        ),
+      },
+    });
+
+    // Filter new transactions
+    const newTransferTransactions = transferTransaction.filter(
+      (transaction: any) =>
+        !existingTransferTransactions.some(
+          (existing: any) =>
+            existing.transaction_code === transaction.transaction_id
+        )
+    );
+
+    const newReceiverTransactions = receiverTransaction.filter(
+      (transaction: any) =>
+        !existingReceiverTransactions.some(
+          (existing: any) =>
+            existing.transaction_code === transaction.transaction_id
+        )
+    );
+    await Promise.all([
+      newTransferTransactions?.map(async (transactionData: any) => {
         const transactionRecord = await transactionRepository.create({
           values: {
-            transaction_code: transactionData.transaction_code,
-            amount: transactionData.amount,
+            transaction_code: transactionData.transaction_id,
+            amount: transactionData.transferAmount,
             description: transactionData.description,
-            from_account_no: transactionData.from_account_no,
-            from_account_name: transactionData.from_account_name,
-            from_bank_name: transactionData.from_bank_name,
+            from_account_no: transactionData.accountNo,
+            from_account_name: transactionData.benAccountName,
+            from_bank_name: transactionData.bankName,
             fund_id: transactionData.fund_id,
           },
         });
         return transactionRecord;
-      })
-    );
-    console.log('Transaction response:', transaction);
+      }),
+      newReceiverTransactions?.map(async (transactionData: any) => {
+        const transactionRecord = await transactionRepository.create({
+          values: {
+            transaction_code: transactionData.transaction_id,
+            amount: transactionData.receiveAmount,
+            description: transactionData.description,
+            from_account_no: transactionData.accountNo,
+            from_account_name: transactionData.benAccountName,
+            from_bank_name: transactionData.bankName,
+            fund_id: transactionData.fund_id,
+          },
+        });
+        return transactionRecord;
+      }),
+    ]);
+    console.log('New Transfer Transactions:', newTransferTransactions);
+    console.log('New Receiver Transactions:', newReceiverTransactions);
+
+    console.log('Transaction response:', message?.data?.transferTransaction);
 
     return { status: 'processed', message: 'Hello received' };
   }
--- lcdp-app/packages/plugins/@dtu-olp-2024/kafka-nocobase/src/server/plugin.ts
+++ lcdp-app/packages/plugins/@dtu-olp-2024/kafka-nocobase/src/server/plugin.ts
@@ -19,278 +19,296 @@ export class KafkaNocobaseServer extends Plugin {
   }
 
   async load() {
-    this.router.post('/api/admin/kafka/emit', async (ctx) => {
-      console.log('ctx.request.body', ctx.request.body);
-      return ctx.request.body;
-    });
-    const db = this.db;
-    const tableKafkaTopicName = 'kafka_topics';
-    const tableKafkaConfigName = 'kafka_configs';
-    const tableKafkaConfigExists = await db.sequelize
-      .getQueryInterface()
-      .showAllTables()
-      .then((tables) => tables.includes(tableKafkaConfigName));
+    try {
+      const db = this.db;
+      const tableKafkaTopicName = 'kafka_topics';
+      const tableKafkaConfigName = 'kafka_configs';
+      const tableKafkaConfigExists = await db.sequelize
+        .getQueryInterface()
+        .showAllTables()
+        .then((tables) => tables.includes(tableKafkaConfigName));
 
-    const tableKafkaTopicExists = await db.sequelize
-      .getQueryInterface()
-      .showAllTables()
-      .then((tables) => tables.includes(tableKafkaTopicName));
+      const tableKafkaTopicExists = await db.sequelize
+        .getQueryInterface()
+        .showAllTables()
+        .then((tables) => tables.includes(tableKafkaTopicName));
 
-    if (!tableKafkaTopicExists) {
-      console.warn('Creating kafka_topics collection');
-      this.db.collection({
-        name: tableKafkaTopicName,
-        fields: [
-          {
-            type: 'uuid',
-            name: 'id',
-            primaryKey: true,
-          },
-          {
-            type: 'string',
-            name: 'broker_host',
-            required: true,
-          },
-          {
-            type: 'string',
-            name: 'topic_name',
-            required: true,
-          },
-          {
-            type: 'string',
-            name: 'type',
-            required: true,
-          },
-        ],
-      });
-
-      this.app.db.getCollection('collections').repository.create({
-        values: {
+      if (!tableKafkaTopicExists) {
+        console.warn('Creating kafka_topics collection');
+        this.db.collection({
           name: tableKafkaTopicName,
-          title: 'Kafka Topics',
           fields: [
             {
-              name: 'id',
               type: 'uuid',
-              interface: 'input',
+              name: 'id',
               primaryKey: true,
-              autoIncrement: false,
-              uiSchema: {
-                type: 'string',
-                title: 'ID',
-                'x-component': 'Input',
-                required: true,
-              },
             },
             {
+              type: 'string',
               name: 'broker_host',
-              interface: 'input',
-              type: 'string',
-              uiSchema: {
-                type: 'string',
-                title: 'Broker Host',
-                'x-component': 'Input',
-                required: true,
-              },
+              required: true,
             },
             {
+              type: 'string',
               name: 'topic_name',
-              interface: 'input',
-              type: 'string',
-              uiSchema: {
-                type: 'string',
-                title: 'Topic Name',
-                'x-component': 'Input',
-                required: true,
-              },
+              required: true,
             },
             {
+              type: 'string',
               name: 'type',
-              interface: 'select',
-              type: 'string',
-              uiSchema: {
-                type: 'string',
-                title: 'Type',
-                'x-component': 'Select',
-                'x-component-props': {
-                  options: [
-                    { label: 'Producer', value: 'producer' },
-                    { label: 'Consumer', value: 'consumer' },
-                  ],
+              required: true,
+            },
+          ],
+        });
+
+        this.app.db.getCollection('collections').repository.create({
+          values: {
+            name: tableKafkaTopicName,
+            title: 'Kafka Topics',
+            fields: [
+              {
+                name: 'id',
+                type: 'uuid',
+                interface: 'input',
+                primaryKey: true,
+                autoIncrement: false,
+                uiSchema: {
+                  type: 'string',
+                  title: 'ID',
+                  'x-component': 'Input',
+                  required: true,
                 },
-                required: true,
               },
-            },
-          ],
-          actions: [
-            {
-              name: 'create',
-              type: 'create',
-              title: 'Create Kafka Topic',
-              viewName: 'form',
-            },
-            {
-              name: 'view',
-              type: 'view',
-              title: 'View',
-              viewName: 'form',
-            },
-            {
-              name: 'edit',
-              type: 'edit',
-              title: 'Edit',
-              viewName: 'form',
-            },
-            {
-              name: 'destroy',
-              type: 'destroy',
-              title: 'Delete',
-            },
-          ],
-        },
-      });
+              {
+                name: 'broker_host',
+                interface: 'input',
+                type: 'string',
+                uiSchema: {
+                  type: 'string',
+                  title: 'Broker Host',
+                  'x-component': 'Input',
+                  required: true,
+                },
+              },
+              {
+                name: 'topic_name',
+                interface: 'input',
+                type: 'string',
+                uiSchema: {
+                  type: 'string',
+                  title: 'Topic Name',
+                  'x-component': 'Input',
+                  required: true,
+                },
+              },
+              {
+                name: 'type',
+                interface: 'select',
+                type: 'string',
+                uiSchema: {
+                  type: 'string',
+                  title: 'Type',
+                  'x-component': 'Select',
+                  'x-component-props': {
+                    options: [
+                      { label: 'Producer', value: 'producer' },
+                      { label: 'Consumer', value: 'consumer' },
+                    ],
+                  },
+                  required: true,
+                },
+              },
+            ],
+            actions: [
+              {
+                name: 'create',
+                type: 'create',
+                title: 'Create Kafka Topic',
+                viewName: 'form',
+              },
+              {
+                name: 'view',
+                type: 'view',
+                title: 'View',
+                viewName: 'form',
+              },
+              {
+                name: 'edit',
+                type: 'edit',
+                title: 'Edit',
+                viewName: 'form',
+              },
+              {
+                name: 'destroy',
+                type: 'destroy',
+                title: 'Delete',
+              },
+            ],
+          },
+        });
 
-      await this.db.sync();
-    } else {
-      console.warn('kafka_topics collection already exists');
-    }
-    if (!tableKafkaConfigExists) {
-      console.warn('Creating kafka_configs collection');
-      this.db.collection({
-        name: tableKafkaConfigName,
-        fields: [
-          {
-            type: 'uuid',
-            name: 'id',
-            primaryKey: true,
-          },
-          {
-            type: 'string',
-            name: 'group_id',
-            required: true,
-          },
-          {
-            type: 'string',
-            name: 'client_id',
-            required: true,
-          },
-        ],
-      });
-
-      this.app.db.getCollection('collections').repository.create({
-        values: {
+        await this.db.sync();
+      } else {
+        console.warn('kafka_topics collection already exists');
+      }
+      if (!tableKafkaConfigExists) {
+        console.warn('Creating kafka_configs collection');
+        this.db.collection({
           name: tableKafkaConfigName,
-          title: 'Kafka Configs',
           fields: [
             {
-              name: 'id',
               type: 'uuid',
-              interface: 'input',
+              name: 'id',
               primaryKey: true,
-              autoIncrement: false,
-              uiSchema: {
-                type: 'string',
-                title: 'ID',
-                'x-component': 'Input',
-                required: true,
-              },
             },
             {
+              type: 'string',
               name: 'group_id',
-              interface: 'input',
-              type: 'string',
-              uiSchema: {
-                type: 'string',
-                title: 'Group Id',
-                'x-component': 'Input',
-                required: true,
-              },
+              required: true,
             },
             {
+              type: 'string',
               name: 'client_id',
-              interface: 'input',
-              type: 'string',
-              uiSchema: {
-                type: 'string',
-                title: 'Client Id',
-                'x-component': 'Input',
-                required: true,
+              required: true,
+            },
+          ],
+        });
+
+        this.app.db.getCollection('collections').repository.create({
+          values: {
+            name: tableKafkaConfigName,
+            title: 'Kafka Configs',
+            fields: [
+              {
+                name: 'id',
+                type: 'uuid',
+                interface: 'input',
+                primaryKey: true,
+                autoIncrement: false,
+                uiSchema: {
+                  type: 'string',
+                  title: 'ID',
+                  'x-component': 'Input',
+                  required: true,
+                },
               },
-            },
-          ],
-          actions: [
-            {
-              name: 'create',
-              type: 'create',
-              title: 'Create Kafka Config',
-              viewName: 'form',
-            },
-            {
-              name: 'view',
-              type: 'view',
-              title: 'View',
-              viewName: 'form',
-            },
-            {
-              name: 'edit',
-              type: 'edit',
-              title: 'Edit',
-              viewName: 'form',
-            },
-            {
-              name: 'destroy',
-              type: 'destroy',
-              title: 'Delete',
-            },
-          ],
+              {
+                name: 'group_id',
+                interface: 'input',
+                type: 'string',
+                uiSchema: {
+                  type: 'string',
+                  title: 'Group Id',
+                  'x-component': 'Input',
+                  required: true,
+                },
+              },
+              {
+                name: 'client_id',
+                interface: 'input',
+                type: 'string',
+                uiSchema: {
+                  type: 'string',
+                  title: 'Client Id',
+                  'x-component': 'Input',
+                  required: true,
+                },
+              },
+            ],
+            actions: [
+              {
+                name: 'create',
+                type: 'create',
+                title: 'Create Kafka Config',
+                viewName: 'form',
+              },
+              {
+                name: 'view',
+                type: 'view',
+                title: 'View',
+                viewName: 'form',
+              },
+              {
+                name: 'edit',
+                type: 'edit',
+                title: 'Edit',
+                viewName: 'form',
+              },
+              {
+                name: 'destroy',
+                type: 'destroy',
+                title: 'Delete',
+              },
+            ],
+          },
+        });
+
+        await this.db.sync();
+      }
+
+      this.app.acl.allow('kafka_topics', '*');
+      this.app.acl.allow('kafka_configs', '*');
+      const kafkaTopics = (
+        await this.db.sequelize.query('SELECT * FROM kafka_topics')
+      )[0];
+      const kafkaConfigs = (
+        await this.db.sequelize.query('SELECT * FROM kafka_configs')
+      )[0][0] as any;
+      const brokers = kafkaTopics
+        .filter((topic: any) => topic.broker_host)
+        .map((topic: any) => topic.broker_host);
+      const topics = kafkaTopics
+        .filter((topic: any) => topic.topic_name)
+        .map((topic: any) => topic.topic_name);
+      if (!brokers.length || kafkaConfigs === undefined) {
+        return console.error('No brokers found');
+      }
+      this.kafka = new Kafka({
+        clientId: kafkaConfigs?.client_id,
+        brokers: brokers,
+        retry: {
+          initialRetryTime: 100,
+          retries: 5,
         },
       });
+      this.producer = this.kafka.producer();
+      this.consumer = this.kafka.consumer({ groupId: kafkaConfigs?.group_id });
+      await Promise.all([this.producer.connect(), this.consumer.connect()]);
+      this.eventListener = new KafkaEventListener(this.producer, this.consumer);
+      await this.eventListener.initializeTopics(topics, this.app);
+      this.app.context.kafkaEmit = this.eventListener.emit.bind(
+        this.eventListener
+      );
+      this.router.post('/api/admin/kafka/send-message', async (ctx) => {
+        try {
+          const { topic_name, data } = ctx.request.body;
 
-      await this.db.sync();
-    }
+          console.log('Sending message to topic:', ctx.request.body);
 
-    this.app.acl.allow('kafka_topics', '*');
-    this.app.acl.allow('kafka_configs', '*');
-    const kafkaTopics = (
-      await this.db.sequelize.query('SELECT * FROM kafka_topics')
-    )[0];
-    const kafkaConfigs = (
-      await this.db.sequelize.query('SELECT * FROM kafka_configs')
-    )[0][0] as any;
-    const brokers = kafkaTopics
-      .filter((topic: any) => topic.broker_host)
-      .map((topic: any) => topic.broker_host);
-    const topics = kafkaTopics
-      .filter((topic: any) => topic.topic_name)
-      .map((topic: any) => topic.topic_name);
-    if (!brokers.length || kafkaConfigs === undefined) {
-      return console.error('No brokers found');
+          if (!topic_name || !topic_name) {
+            ctx.status = 400;
+            return (ctx.body = {
+              success: false,
+              error: 'Topic and message are required',
+            });
+          }
+
+          await this.producer.send({
+            topic: topic_name,
+            messages: [{ value: JSON.stringify(data) }],
+          });
+        } catch (error) {}
+      });
+    } catch (error) {
+      console.error('Error during Kafka initialization:', error);
     }
-    this.kafka = new Kafka({
-      clientId: kafkaConfigs?.client_id,
-      brokers: brokers,
-      retry: {
-        initialRetryTime: 100,
-        retries: 5,
-      },
-    });
-    this.producer = this.kafka.producer();
-    this.consumer = this.kafka.consumer({ groupId: kafkaConfigs?.group_id });
-    await Promise.all([this.producer.connect(), this.consumer.connect()]);
-    this.eventListener = new KafkaEventListener(this.producer, this.consumer);
-    await this.eventListener.initializeTopics(topics, this.app);
-    this.app.context.kafkaEmit = this.eventListener.emit.bind(
-      this.eventListener
-    );
   }
 
   async install(options?: InstallOptions) {
-    // Create tables if they don't exist
     const db = this.db;
     const tableKafkaTopicName = 'kafka_topics';
     const tableKafkaConfigName = 'kafka_configs';
 
-    // Create collections
     this.db.collection({
       name: tableKafkaTopicName,
       fields: [
@@ -310,10 +328,8 @@ export class KafkaNocobaseServer extends Plugin {
       ],
     });
 
-    // Sync database
     await this.db.sync();
 
-    // Insert default config if needed
     const hasConfig = await db.getRepository('kafka_configs').count();
     if (!hasConfig) {
       await db.getRepository('kafka_configs').create({
@@ -324,7 +340,6 @@ export class KafkaNocobaseServer extends Plugin {
       });
     }
 
-    // Insert default topic if needed
     const hasTopic = await db.getRepository('kafka_topics').count();
     if (!hasTopic) {
       await db.getRepository('kafka_topics').create({
@@ -336,7 +351,6 @@ export class KafkaNocobaseServer extends Plugin {
       });
     }
 
-    // Set permissions
     this.app.acl.allow('kafka_topics', '*');
     this.app.acl.allow('kafka_configs', '*');
 
