5b7e5ef56ffc1105a833a5f8f5cf01c0218ecb1f
Trần Nguyễn Duy Khánh <147808163+AugustusDngQt@users.noreply.github.com>
2024-12-07 16:56:29 +0700

    fix: action change height for progress & rich block

--- lcdp-app/packages/plugins/@dtu-olp-2024/rich-block-nocobase/src/client/schema/infoItem.tsx
+++ lcdp-app/packages/plugins/@dtu-olp-2024/rich-block-nocobase/src/client/schema/infoItem.tsx
@@ -3,12 +3,12 @@ import { ISchema } from '@nocobase/client';
 import { useHeaderPickerProps } from '.';
 import { useFieldSchema } from '@formily/react';
 import { uid } from '@formily/shared';
-import { headerTypeSettings } from '../settings/items/headerType';
+import { richTextSettings } from '../settings/items/richText';
 
 export const InfoField: React.FC<{ name: string }> = ({ name }) => {
   const { data, loading } = useHeaderPickerProps();
   const schema = useFieldSchema();
-  const headerType = schema?.['x-component-props']?.headerType || 'h1';
+  const richText = schema?.['x-component-props']?.richText || 'h1';
 
   const displayValue = React.useMemo(() => {
     if (!data) return '';
@@ -19,7 +19,7 @@ export const InfoField: React.FC<{ name: string }> = ({ name }) => {
     return data[name];
   }, [data, name]);
 
-  const Component = headerType as keyof JSX.IntrinsicElements;
+  const Component = richText as keyof JSX.IntrinsicElements;
 
   if (loading) return <Component>Loading...</Component>;
   return <Component>{displayValue || 'N/A'}</Component>;
@@ -41,9 +41,9 @@ export function getInfoItemSchema({ name }: { name: string }) {
             'x-component': 'InfoField',
             'x-component-props': {
               name,
-              headerType: 'h1',
+              richText: 'h1',
             },
-            'x-settings': headerTypeSettings.name,
+            'x-settings': richTextSettings.name,
             'x-decorator': 'FormItem',
           },
         },
